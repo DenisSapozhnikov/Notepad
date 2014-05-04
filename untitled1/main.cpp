da6797a1b86454ae55a1d01973eaa54344cf0b6a
samikhaylov <ismikhaylov@mail.ru>
2014-05-04 20:09:56 +0400

    first commit

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -1,1 +1,12 @@
+#include <QApplication>
+#include <QLabel>
 
+int main(int argc, char * argv[])
+{
+    QApplication app(argc, argv);              // QApp... instead of Qapp...
+    QLabel *label = new QLabel("hello world"); // QLabel * instead of Qlabel
+
+    label->show();  // <- label-> instead of label.
+
+    return app.exec();
+}
